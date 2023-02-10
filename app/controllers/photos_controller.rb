966c8dad9662eac472756e00d538f935c903c1f1
scwang07 <swang20@chicagobooth.edu>
2023-02-10 01:01:51 +0000

    Basic framework for photos and photo details -- no forms yet

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,17 @@
 class PhotosController < ApplicationController
   def index
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({:created_at => :desc})
+
     render({:template => "photo_templates/index.html.erb"})
   end
 
   def photo_details
+    photo_id = params.fetch("photo_id")
+    @list_of_comments = Comment.where({:photo_id => photo_id})
+
+    matching_photos = Photo.where({:id => photo_id})
+    @the_photo = matching_photos.first
     render({:template => "photo_templates/details.html.erb"})
   end
 end
